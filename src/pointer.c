d875531b0a5db1a5cc1294ac3089881162bd5366
Sheikh Qumruzzaman <squmruzz@cisco.com>
2020-03-06 17:20:30 -0800

    initial import

---
+++ src/pointer.c
@@ -0,0 +1,58 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mcheck.h>
+
+typedef struct
+{
+    char        *street;
+    char        *city;
+    int         zip;
+} address;
+
+typedef struct
+{
+    char        *name;
+    address     *addr;
+} person;
+
+void test_trace();
+void print_struct(void *v, void *arg);
+person* create_person(char *name, char *street, char *city, int zip);
+
+void print_struct(void *v, void *arg)
+{
+    person *p = (person*) v;
+    printf("%s\n%s\n%s\n%d\n", p->name, p->addr->street, p->addr->city, p->addr->zip);
+}
+
+person* create_person(char *name, char *street, char *city, int zip)
+{
+    person      *p;
+    address     *addr;
+
+    p = calloc(1, sizeof(person));
+    addr = calloc(1, sizeof(address));
+
+    p->name = name;
+    addr->street = street;
+    addr->city = city;
+    addr->zip = zip;
+    p->addr = addr;
+    
+    return p;
+}
+
+int main (void)
+{
+    mtrace();
+    //char*   name = calloc(1000, sizeof(char));
+    char*   name = malloc(1000 * sizeof(char));
+    name = "Welcome!";
+    puts(name);
+    
+    person *p = create_person("Sheikh Umayer", "110 Kiely Blvd", "Santa Clara", 95051);
+    print_struct(p, NULL);
+
+    return 0;
+}
