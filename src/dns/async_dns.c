3a296f5176d7c48432f71fe1b323fc16bc97982a
Sheikh Qumruzzaman <alamin141@gmail.com>
2020-09-11 15:12:41 -0400

    Moved event base to dns context structure

--- src/dns/async_dns.c
+++ src/dns/async_dns.c
@@ -13,52 +13,69 @@
 
 #define DNS_TIMEOUT_MS              4000
 #define MAX_NAMESERVERS             16
+#define USE_BITMASK					0
 
-struct event_base 				*base;
 ares_channel					channel;
 struct ares_addrinfo_hints* 	hint;
 
-static void
-ares_read_cb (evutil_socket_t  sockfd, short event, void *args)
+typedef struct dns_ctx
 {
-	printf("ares_read_cb\n");
+	struct event_base 				*base;
+	ares_channel					channel;
+	struct ares_addrinfo_hints* 	hint;
+} t_dns_ctx;
+
+static t_dns_ctx		dns_ctx;
+
+t_dns_ctx *
+get_dns_ctx(void)
+{
+	return &dns_ctx;
+}
+
+static void
+ares_ev_cb (evutil_socket_t  sockfd, short event, void *args)
+{
+	printf("ares_ev_cb\n");
 	ares_channel   channel = args;
 
     if (event & EV_TIMEOUT) {
 		printf("EV_TIMEOUT\n");
-        return ares_process_fd(channel, ARES_SOCKET_BAD, ARES_SOCKET_BAD);
+        ares_process_fd(channel, sockfd, ARES_SOCKET_BAD);
 
     } else if (event & EV_READ) {
 		printf("EV_READ\n");
-        return ares_process_fd(channel, sockfd, ARES_SOCKET_BAD);
+        ares_process_fd(channel, sockfd, ARES_SOCKET_BAD);
     }
 }
 
 
-
 static void
-state_cb(void *data, int fd, int read, int write)
+ares_state_cb(void *data, int fd, int read, int write)
 {
     printf("Change state fd %d read:%d write:%d\n", fd, read, write);
+	t_dns_ctx *dns_ctx_p = get_dns_ctx();
 
-	int						bitmask;
+	if(USE_BITMASK) {
+		return;
+	}
+
 	struct timeval			tv, *timeout;
     struct event 			*read_evt;
 
 	timeout = ares_timeout(channel, NULL, &tv);
 
-	printf("Adding event for FD: %d\n", fd);
-	
 	if (read) {
-		read_evt = event_new(base, fd, EV_PERSIST | EV_READ | EV_TIMEOUT, ares_read_cb, channel);
+		printf("Adding event for FD: %d\n", fd);
+		read_evt = event_new(dns_ctx_p->base, fd, EV_PERSIST | EV_READ | EV_TIMEOUT, ares_ev_cb, channel);
 		event_add(read_evt, timeout);
 	}
 }
 
 static void
-callback(void *arg, int status, int timeouts, struct ares_addrinfo *host)
+ares_res_cb(void *arg, int status, int timeouts, struct ares_addrinfo *host)
 {
-	printf("callback:\n");
+	printf("ares_res_cb\n");
     struct ares_addrinfo_node   *p_node;
 
     if(!host || status != ARES_SUCCESS){
@@ -80,19 +97,11 @@ callback(void *arg, int status, int timeouts, struct ares_addrinfo *host)
     }
 }
 
-void
-event_init()
-{
-    base = event_base_new();
-}
-
-void ares_init_mine() { }
-
+/*
 void
 ares_create_event(ares_channel channel)
 {
-	int						socks[MAX_NAMESERVERS];
-	int						bitmask;
+	int						bitmask, socks[MAX_NAMESERVERS];
 	struct timeval			tv, *timeout;
     struct event 			*read_evt[MAX_NAMESERVERS];
 
@@ -105,27 +114,36 @@ ares_create_event(ares_channel channel)
 			printf("%d is readable.\n", i);
             read_evt[j] = event_new(base, socks[i], 
                                     EV_PERSIST | EV_READ | EV_TIMEOUT,
-                                    ares_read_cb, channel);
+                                    ares_ev_cb, channel);
 			printf("Adding event %d.\n", i);
 			event_add(read_evt[j++], timeout);
         }
+    }
+}
+*/
+
+void
+ares_init2()
+{
+	int status;
+
+    status = ares_library_init(ARES_LIB_INIT_ALL);
+    if (status != ARES_SUCCESS){
+        printf("ares_library_init: %s\n", ares_strerror(status));
     }
 }
 
 int
 main(void)
 {
-	event_init();
+	t_dns_ctx *dns_ctx_p = get_dns_ctx();
+    dns_ctx_p->base = event_base_new();
+
+	ares_init2();
 
     struct ares_options				options;
     struct ares_addrinfo_hints 		hints;
     int 							status, optmask = 0;
-
-    status = ares_library_init(ARES_LIB_INIT_ALL);
-    if (status != ARES_SUCCESS){
-        printf("ares_library_init: %s\n", ares_strerror(status));
-        return 1;
-    }
 
     hints.ai_family     = AF_UNSPEC;
     hints.ai_socktype   = SOCK_DGRAM;
@@ -135,7 +153,7 @@ main(void)
     
     options.timeout = DNS_TIMEOUT_MS;
     options.tries = 1;
-    options.sock_state_cb = state_cb;
+    options.sock_state_cb = ares_state_cb;
 
     optmask |= ARES_OPT_TIMEOUTMS;
     optmask |= ARES_OPT_TRIES;
@@ -148,12 +166,14 @@ main(void)
     }
 
     //ares_gethostbyname(channel, "vbond.cisco.com", AF_INET, callback, NULL);
-    ares_getaddrinfo(channel, "centos.com", NULL, hint , callback, NULL);
+    ares_getaddrinfo(channel, "centos.com", NULL, hint , ares_res_cb, NULL);
 
-   	//ares_create_event(channel); 
+	if (USE_BITMASK) {
+		//ares_create_event(channel); 
+	}
 
     //wait_ares(channel);
-	event_base_dispatch(base);
+	event_base_dispatch(dns_ctx_p->base);
     ares_destroy(channel);
     ares_library_cleanup();
     printf("fin\n");
